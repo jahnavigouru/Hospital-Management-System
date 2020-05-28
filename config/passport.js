230d7c8ef4e64054af6bd6af49df79e541b30e6c
JAHNAVI GOURU <jahnavi0801@gmail.com>
2020-05-28 18:41:06 +0530

    modified

--- config/passport.js
+++ config/passport.js
@@ -1,21 +1,26 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
+const mysql = require('mysql');
 
 //DB 
-const db = require('../app')
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'HospitalManagment'
+})
 
 module.exports = function(passport) {
     passport.use(
-        new LocalStrategy({ usernameField: 'email', passReqToCallback : true }, (email, password, done) => {
-
+        new LocalStrategy({ usernameField: 'email', passReqToCallback : true }, function (req, email, password, done)  {
             //Match User
-            let check = `select * from users where UserID = "${email}"`
-            db.query(check, (err, match) => {
+                let check = `select * from users where UserID = "${email}"`
+                db.query(check, (err, match) => {
+
                 if(err) throw err
                 if(match.length == 0) {
                     return done(null, false, { message: `${email} is not registered` })
                 }
-
                 //Match Password
                 bcrypt.compare(password, match[0].password, (err, isMatch) => {
                     if(err) throw err
@@ -27,15 +32,15 @@ module.exports = function(passport) {
                 })
             })
         })
-    )
+    ) 
+    
     passport.serializeUser((user, done) => {
         done(null, user.id);
-      })
-    
+      })    
       passport.deserializeUser((id, done) => {
            let sql = `select id from users where id = ${id}`
            db.query(sql, (err, result) => {
                done(err, result[0])
            })
-      })
+      })  
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-
 const router = express.Router()
+const { ensureAuthenticated } = require('../config/auth')
 
+
+//Welcome Page
 router.get('/', (req, res) => res.render('welcome'))
-router.get('/dashboard', (req, res) => res.render('dashboard'))
+// Dashboard Page
+router.get('/dashboard',  ensureAuthenticated, (req, res) => res.render('dashboard'))
 
 
 module.exports = router
